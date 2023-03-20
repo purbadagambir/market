82bf49de6feb95bc6286135bf62debfbf7db794e
Henry Purba <purbadagambir@gmail.com>
2023-03-20 14:42:45 +0700

    can access with https

--- sources/config/app.php
+++ sources/config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://pondo.co.id/market/'),
 
     'asset_url' => env('ASSET_URL', null),
 
